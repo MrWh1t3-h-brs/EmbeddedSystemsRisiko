f2578c24a34b4abcc62c7294748c7c4457456879
MrWh1t3-h-brs <Mr.Wh1t3@web.de>
2017-12-20 11:54:47 +0100

    Zusammenfassung
    Variable NameTest wurde in Test geändert

---
+++ Verwaltung.java
@@ -0,0 +1,28 @@
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 17.11.2017
+  * @author 
+  */
+
+public class Verwaltung {
+  
+  // Anfang Attribute
+  // Ende Attribute
+  
+  // Anfang Methoden
+  public void main() {
+    
+  }
+
+  public void GebieteMischen() {
+    
+  }
+
+  public void GebieteAnSpielerVerteilen() {
+    
+  }
+
+  // Ende Methoden
+} // end of Verwaltung
